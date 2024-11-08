64cbb92c81ce4cefe7edf07ab8d06d54b2467d1c
AleCintas02 <alecintas003@hotmail.com>
2024-11-08 13:21:35 -0300

    Gestion biblioteca terminado

--- GestionBiblioteca.java
+++ GestionBiblioteca.java
@@ -2,21 +2,6 @@ import java.util.*;
 
 public class GestionBiblioteca {
     public static void main(String[] args) throws LibroNoPrestadoException {
-        
-        
-        /*
-        Cargar Libro
-        Dar de Baja un Libro
-        Cargar Socio
-        Dar de baja Socio
-        Prestar Libro
-        Devolver Libro"+
-        Lista de Libros"+
-        Buscar Libro"+
-        Docentes Responsables
-        Lista Socios
-        * 
-        */
         HashMap<Integer, Socio> socios = new HashMap<>();
         ArrayList<Libro> libros = new ArrayList<>();
         
@@ -32,7 +17,7 @@ public class GestionBiblioteca {
         socios.put(docente2.getDniSocio(), docente2);
 
         
-        Libro libro1 = new Libro("Java para Principiantes", 1, "Editorial X", 2020);
+        Libro libro1 = new Libro("Programando con JAVA”", 1, "Editorial X", 2020);
         Libro libro2 = new Libro("Introducción a la Programación", 3, "Editorial Y", 2018);
         Libro libro3 = new Libro("Patrones de Diseño", 2, "Editorial Z", 2021);
         Libro libro4 = new Libro("Bases de Datos Avanzadas", 4, "Editorial W", 2019);
@@ -41,26 +26,23 @@ public class GestionBiblioteca {
         libros.add(libro3);
         libros.add(libro4);
 
-        
-    
-        Calendar fechaRetiro = Calendar.getInstance();
-
-        
         Biblioteca biblioteca1 = new Biblioteca("Libros de Lujo", socios, libros);
+        System.out.println("Cantidad de socios de tipo Estudiante: "+biblioteca1.cantidadDeSociosPortipo("Estudiante"));
+        
+        System.out.println("\nLista de docentes que nunca han adeudado ni adeudan libros: ");
+        System.out.println(biblioteca1.docentesResponsables());
+        
+        System.out.println("\n\n\nLista de libros y socios:");
         System.out.println(biblioteca1.listaDeLibros());
         System.out.println(biblioteca1.listaDeSocios());
         
         
-        System.out.println("\n\nLibro prestado");
+        
+        Calendar fechaRetiro = Calendar.getInstance();
         biblioteca1.prestarLibro(fechaRetiro, estudiante1, libro1);
-        System.out.println(biblioteca1.listaDeLibros());
-        System.out.println(biblioteca1.listaDeSocios());
-        
-        
-        System.out.println("\n\nLibro devuelto");
-        biblioteca1.devolverLibro(libro1);
-        System.out.println(biblioteca1.listaDeLibros());
-        System.out.println(biblioteca1.listaDeDocentesResponsables());
+        System.out.println("\n\n\n¿Qué socio tiene prestado el libro “Programando con JAVA”? ");
+        System.out.println(biblioteca1.quienTieneElLibro(libro1));
+  
 
     }
 }
